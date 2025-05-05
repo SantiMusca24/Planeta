de472a1157115a138ca772219e15eb14c8654747
SantiMusca24 <santiago.muscatiello@gmail.com>
2025-05-05 00:41:05 -0300

    Escena arboleada + Objetivos + Spawneo por lvl en ambas escenas

--- Assets/Scripts/ContadorUI.cs
+++ Assets/Scripts/ContadorUI.cs
@@ -18,6 +18,11 @@ public class ContadorUI : MonoBehaviour
     [SerializeField] private string upgradeNameToTrack = "Upgrade_1";
     [SerializeField] private int goalLevel = 10;
     [SerializeField] private TMP_Text upgradeGoalText;
+    [SerializeField] private TMP_Text GoalText2;
+    [SerializeField] private TMP_Text upgradeGoalText2;
+    [SerializeField] private int goalPoints2 = 5000;
+    [SerializeField] private string upgradeNameToTrack2 = "Upgrade_2";
+    [SerializeField] private int goalLevel2 = 20;
 
     void Start()
     {
@@ -27,6 +32,9 @@ public class ContadorUI : MonoBehaviour
         {
             scrollRectToReset.verticalNormalizedPosition = 1f;
         }
+
+        if (GoalText2 != null) GoalText2.gameObject.SetActive(false);
+        if (upgradeGoalText2 != null) upgradeGoalText2.gameObject.SetActive(false);
     }
 
     public void UpdateUI()
@@ -70,6 +78,38 @@ public class ContadorUI : MonoBehaviour
             else
             {
                 upgradeGoalText.color = Color.white;
+            }
+        }
+        bool firstGoalCompleted = false;
+        bool firstUpgradeCompleted = false;
+
+        if (GoalText != null && GoalText.color == Color.green)
+            firstGoalCompleted = true;
+
+        if (upgradeGoalText != null && upgradeGoalText.color == Color.green)
+            firstUpgradeCompleted = true;
+
+        if (firstGoalCompleted && firstUpgradeCompleted)
+        {
+            
+            if (GoalText != null) GoalText.gameObject.SetActive(false);
+            if (upgradeGoalText != null) upgradeGoalText.gameObject.SetActive(false);
+
+            
+            if (GoalText2 != null)
+            {
+                GoalText2.gameObject.SetActive(true);
+                int currentPoints = Mathf.RoundToInt(GameManager.Instance.count);
+                GoalText2.text = "Puntos: " + currentPoints + " / " + goalPoints2;
+                GoalText2.color = (currentPoints >= goalPoints2) ? Color.green : Color.white;
+            }
+
+            if (upgradeGoalText2 != null)
+            {
+                upgradeGoalText2.gameObject.SetActive(true);
+                int currentLevel2 = PlayerPrefs.GetInt(upgradeNameToTrack2 + "_Level", 0);
+                upgradeGoalText2.text = "Mejora Árboles: " + upgradeNameToTrack2 + " nivel " + currentLevel2 + " / " + goalLevel2;
+                upgradeGoalText2.color = (currentLevel2 >= goalLevel2) ? Color.green : Color.white;
             }
         }
     }
