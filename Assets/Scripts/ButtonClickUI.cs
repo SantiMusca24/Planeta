f5fbfd0a44b7b5c588254976adeb2a8817730127
SantiMusca24 <santiago.muscatiello@gmail.com>
2025-06-21 19:18:12 -0300

    Fix UI

--- Assets/Scripts/ButtonClickUI.cs
+++ Assets/Scripts/ButtonClickUI.cs
@@ -6,9 +6,19 @@ using UnityEngine.EventSystems;
 
 public class ButtonClickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+     [Header("General")]
+    [SerializeField] private bool animateOnEnable = true;
+    [SerializeField] private bool animateOnClick = true;
+
+    [Header("Appear Animation")]
+    [SerializeField] private float appearDuration = 0.5f;
+    [SerializeField] private Vector3 startScale = new Vector3(0.5f, 0.5f, 0.5f);
+    [SerializeField] private Ease appearEase = Ease.OutBack;
+
+    [Header("Click Animation")]
     [SerializeField] private float pressedScale = 1.1f;
-    [SerializeField] private float animationDuration = 0.1f;
-    [SerializeField] private Ease animationEase = Ease.OutQuad;
+    [SerializeField] private float clickDuration = 0.1f;
+    [SerializeField] private Ease clickEase = Ease.OutQuad;
 
     private Vector3 originalScale;
 
@@ -17,15 +27,27 @@ public class ButtonClickUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         originalScale = transform.localScale;
     }
 
+    private void OnEnable()
+    {
+        if (!animateOnEnable) return;
+
+        transform.localScale = startScale;
+        transform.DOScale(originalScale, appearDuration).SetEase(appearEase);
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (!animateOnClick) return;
+
         transform.DOKill();
-        transform.DOScale(originalScale * pressedScale, animationDuration).SetEase(animationEase);
+        transform.DOScale(originalScale * pressedScale, clickDuration).SetEase(clickEase);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (!animateOnClick) return;
+
         transform.DOKill();
-        transform.DOScale(originalScale, animationDuration).SetEase(animationEase);
+        transform.DOScale(originalScale, clickDuration).SetEase(clickEase);
     }
 }
