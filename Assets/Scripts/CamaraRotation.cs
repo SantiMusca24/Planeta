fd138b1c319be8748a7eba9b441a54cad93c7723
SantiMusca24 <santiago.muscatiello@gmail.com>
2025-06-25 22:18:16 -0300

    Revert "fix"
    This reverts commit bfeb5fac335da8a8137cf24c009615194bf8487f.

--- Assets/Scripts/CamaraRotation.cs
+++ Assets/Scripts/CamaraRotation.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class CamaraRotation : MonoBehaviour
 {
@@ -45,10 +44,6 @@ public class CamaraRotation : MonoBehaviour
     public RectTransform blockingPanel;
     public Vector2 panelOnScreenPosition;
     public float panelThreshold = 5f;
-    [Header("Efecto de Fuego")]
-    public Transform fireObject; // el objeto al que hay que acercarse
-    public float fireActivationDistance = 10f;
-    public ScriptableRendererFeature fireFeature;
 
     void Start()
     {
@@ -70,13 +65,8 @@ public class CamaraRotation : MonoBehaviour
     {
         if (target == null) return;
 
-        if (IsCameraBlockedByUI()) return;
-        if (fireObject != null && fireFeature != null)
-        {
-            float distanceToFire = Vector3.Distance(transform.position, fireObject.position);
-            bool shouldEnable = distanceToFire <= fireActivationDistance;
-            fireFeature.SetActive(shouldEnable);
-        }
+        if (IsCameraBlockedByUI()) return; 
+
         if (Input.GetMouseButton(0))
         {
             x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
