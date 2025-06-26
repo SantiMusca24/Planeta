cf61c24803bebd918d20615b96b6f742eceda1b6
w <manuel.pereiro@davinci.edu.ar>
2025-06-25 23:02:14 -0300

    Update CamaraRotation.cs

--- Assets/Scripts/CamaraRotation.cs
+++ Assets/Scripts/CamaraRotation.cs
@@ -67,7 +67,7 @@ public class CamaraRotation : MonoBehaviour
 
         if (IsCameraBlockedByUI()) return; 
 
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(0))
         {
             x += Input.GetAxis("Mouse X") * xSpeed * Time.deltaTime;
             y -= Input.GetAxis("Mouse Y") * ySpeed * Time.deltaTime;
