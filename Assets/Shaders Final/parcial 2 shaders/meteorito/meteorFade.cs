d727e7c9bc41fb310157d9e46e01b4e406e456f0
LoremzoMarmol <lorenzodmarmol@gmail.com>
2025-08-06 01:08:19 -0300

    bomba

---
+++ Assets/Shaders Final/parcial 2 shaders/meteorito/meteorFade.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class meteorFade : MonoBehaviour
+{
+    
+    //private disolve = 1f;
+    private float change = 0.1f;
+    public Material disolve;
+    public float someValue = 0;
+    public GameObject stigma;
+    public bool begin = false;
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        begin = false;
+        someValue = 0;
+    }
+
+    /*
+    void Changeshader()
+    {
+        if (shaderValue <= 0f)
+        {
+            CancelInvoke();
+        }
+
+        else if (door.GetComponent<MeshRenderer>().material = changeDisolve)
+        {
+            shaderValue -= change;
+            changeDisolve.SetFloat("Visibility", shaderValue);
+        }
+    }
+    */
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        //print("hello 1");
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0) && !begin)
+        {
+
+            if (hit.transform.position == stigma.transform.position)
+            {
+                begin = true;
+            }
+        }
+        if (someValue >= 1)
+        {
+            StopCoroutine("Decrease");
+            someValue = 0;
+        }
+
+        stigma.GetComponent<Renderer>().material.SetFloat("_disolver", someValue);
+
+        //coroutines aren't exclusive, so this makes sure it is only run one time
+        if (begin == true)
+        {
+            //calls the coroutine
+            StartCoroutine("Decrease");
+            begin = false;
+        }
+    }
+
+    //coroutine decleration
+    IEnumerator Decrease()
+    {
+
+        while (someValue < 1)
+        {
+            someValue += 00.05f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    
+}
+
+
